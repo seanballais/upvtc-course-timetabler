dc9cd9ccaf13a9ab879cf13482fd8c6162f56a44
Sean Francis N. Ballais <sean@seanballais.com>
2019-10-15 13:17:33 +0800

    :whale: Finished modifying the Docker Compose file. Production Compose file is still untested.

--- upvtc_ct/settings.py
+++ upvtc_ct/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [ 'localhost', '127.0.0.1' ]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'core',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
