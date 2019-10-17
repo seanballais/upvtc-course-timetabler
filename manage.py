0734aff4e08daaf18e3729c7abfa20b246546b9f
Sean Francis N. Ballais <sean@seanballais.com>
2019-10-17 14:04:29 +0800

    :page_facing_up: :wrench: Modified LICENSE to include license of projects whose code has been used here. Added additional environment variables and a feature to read and set environmental variables from an environment file to allow the project or any of the project scripts (e.g. manage.py) to be ran in non-Docker container environments.

--- manage.py
+++ manage.py
@@ -4,6 +4,42 @@ import os
 import sys
 
 
+def read_env_file(env_file: str):
+    # Based on `manage.py` in https://gist.github.com/bennylope/2999704, but
+    # with the parser being implemented differently.
+    try:
+        with open(env_file) as f:
+            content = f.read()
+    except IOError as e:
+        print(f"❌ Error reading environment file '{env_file}': {str(e)}")
+        sys.exit(-1)
+
+    line_number = 0
+    for line in content.splitlines():
+        line_number += 1
+
+        if line.startswith('#'):
+            # Line is a comment.
+            continue
+
+        try:
+            key, value = line.lstrip().split('=', 1)
+        except ValueError:
+            print(f"❌ Key-value pair at line {line_number} is invalid.")
+            sys.exit(-1)
+
+        # Remove string definition quotations, if any, from the variable value
+        # since they're not supposed to be in the value.
+        #
+        # TODO: Add string definition quotation validations just like in
+        #       shells, such as Bash and ZSH.
+        if (value.startswith('"') and value.endswith('"') or
+                value.startswith('\'') and value.endswith('\'')):
+            value = value[1:-1]
+
+        os.environ.setdefault(key, value)
+
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upvtc_ct.settings')
     try:
@@ -14,6 +50,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    read_env_file('.config/web.env')
+    read_env_file('.config/db.env')
     execute_from_command_line(sys.argv)
 
 
