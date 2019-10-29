7702852a685de90b8f0b0509551fc16894119c71
Sean Francis N. Ballais <sean@seanballais.com>
2019-10-29 09:06:34 +0800

    Added a scheduler app.

---
+++ scheduler/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from upvtc_ct import utils
+
+
+class Schedule(utils.Base):
+	pass
+
+
+class Division(utils.Base):
+	pass
+
+
+class Class(utils.Base):
+	pass
+
+
+class Room(utils.Base):
+	pass
+
+
+class RoomFeature(utils.Base):
+	pass
+
+
+class Student(utils.Base):
+	pass
+
+
+class Teacher(utils.Base):
+	pass
