2b870d02f3dea687a60f03398eab42dccae42acf
Sean Francis N. Ballais <sean@seanballais.com>
2019-10-29 09:42:28 +0800

    Rewriting project to a desktop GUI app.

--- prod-docker-compose.sh
+++
@@ -1,5 +0,0 @@
-# Script to be used for productions. Removes the necessity of specifying the
-# Docker Compose files.
-#!/bin/sh
-
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml $@
--- upvtc_ct/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.views import LoginView
-from django.urls import path
-
-urlpatterns = [
-	path('login', LoginView.as_view(
-		template_name='users/login.html',
-		redirect_authenticated_user=True), name='login')
-]
