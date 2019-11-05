fd1662b4d8a22783a40cad3392d637a868e1e5e9
Sean Francis N. Ballais <sfballais123@gmail.com>
2019-11-05 23:53:18 +0800

    :lipstick: Added a rudimentary add dialog that changes its UI elements and window properties based on the model that it is creating a new record for. Still needs more work.

--- upvtc_ct/gui.py
+++ upvtc_ct/gui.py
@@ -1,7 +1,59 @@
-from PyQt5.QtCore import QSize
+import peewee
+from PyQt5.QtCore import QSize, pyqtSlot
 from PyQt5.QtWidgets import (
-	QMainWindow, QFrame, QLabel, QListWidget, QHBoxLayout,
-	QVBoxLayout, QPushButton)
+	QMainWindow, QDialog, QFrame, QHBoxLayout, QVBoxLayout, QLabel,
+	QListWidget, QPushButton, QSpinBox)
+
+from upvtc_ct import models
+
+
+class AddRecordDialog():
+	cached_add_record_windows = dict()
+
+	@classmethod
+	def for_model(cls, model: str, attrs=[]):
+		"""
+		Creates a new dialog for creating new records of a model. If a dialog
+		has already been created beforehand, we use the previously created
+		dialog.
+		"""
+		if model in cls.cached_add_record_windows:
+			return cls.cached_add_record_windows[model]
+		else:
+			dialog = QDialog()
+			dialog.setModal(True)
+			dialog.setWindowTitle(f'Add {model}')
+
+			# Add in the GUI equivalents of the fields in the model.
+			dialog_layout = QVBoxLayout(dialog)
+			#dialog.setLayout(dialog_layout)
+
+			m = getattr(models, model)
+			for attr in attrs:
+				model_attr = getattr(m, attr)
+				attr_type = type(model_attr)
+				attr_layout = None
+				if attr_type is peewee.ManyToManyField:
+					raise NotImplementedError()
+				elif attr_type is peewee.ForeignKeyField:
+					raise NotImplementedError()
+				elif attr_type is peewee.SmallIntegerField:
+					attr_layout = QHBoxLayout()
+
+					capitalize_func = lambda s: s.capitalize()
+					attr_label = QLabel(
+						' '.join(list(map(capitalize_func, attr.split('_')))))
+					attr_layout.addWidget(attr_label)
+
+					attr_layout.addStretch(1)
+
+					attr_spinbox = QSpinBox()
+					attr_spinbox.setMinimum(0)
+					attr_layout.addWidget(attr_spinbox)
+
+				dialog_layout.addLayout(attr_layout)
+
+			return dialog
 
 
 class MainWindow(QMainWindow):
@@ -24,17 +76,17 @@ class MainWindow(QMainWindow):
 		study_plan_panel_title = QLabel('Study Plans')
 		study_plan_panel_layout.addWidget(study_plan_panel_title)
 
-		self.study_plan_list = QListWidget()
-		study_plan_panel_layout.addWidget(self.study_plan_list)
+		study_plan_list = QListWidget()
+		study_plan_panel_layout.addWidget(study_plan_list)
 
 		study_plan_action_btn_layout = QHBoxLayout()
-		self.remove_study_plan_btn = QPushButton('Remove')
-		self.edit_study_plan_btn = QPushButton('Edit')
-		self.add_study_plan_btn = QPushButton('Add')
+		remove_study_plan_btn = QPushButton('Remove')
+		edit_study_plan_btn = QPushButton('Edit')
+		add_study_plan_btn = QPushButton('Add')
 		study_plan_action_btn_layout.addStretch(1)
-		study_plan_action_btn_layout.addWidget(self.remove_study_plan_btn)
-		study_plan_action_btn_layout.addWidget(self.edit_study_plan_btn)
-		study_plan_action_btn_layout.addWidget(self.add_study_plan_btn)
+		study_plan_action_btn_layout.addWidget(remove_study_plan_btn)
+		study_plan_action_btn_layout.addWidget(edit_study_plan_btn)
+		study_plan_action_btn_layout.addWidget(add_study_plan_btn)
 		study_plan_panel_layout.addLayout(study_plan_action_btn_layout)
 
 		# Create the panel that will display the subjects required in a
@@ -46,17 +98,17 @@ class MainWindow(QMainWindow):
 		study_plan_subjects_panel_layout.addWidget(
 			study_plan_subjects_panel_title)
 
-		self.subject_list = QListWidget()
-		study_plan_subjects_panel_layout.addWidget(self.subject_list)
+		subject_list = QListWidget()
+		study_plan_subjects_panel_layout.addWidget(subject_list)
 
 		subject_action_btn_layout = QHBoxLayout()
-		self.remove_subject_btn = QPushButton('Remove')
-		self.edit_subject_btn = QPushButton('Edit')
-		self.add_subject_btn = QPushButton('Add')
+		remove_subject_btn = QPushButton('Remove')
+		edit_subject_btn = QPushButton('Edit')
+		add_subject_btn = QPushButton('Add')
 		subject_action_btn_layout.addStretch(1)
-		subject_action_btn_layout.addWidget(self.remove_subject_btn)
-		subject_action_btn_layout.addWidget(self.edit_subject_btn)
-		subject_action_btn_layout.addWidget(self.add_subject_btn)
+		subject_action_btn_layout.addWidget(remove_subject_btn)
+		subject_action_btn_layout.addWidget(edit_subject_btn)
+		subject_action_btn_layout.addWidget(add_subject_btn)
 		study_plan_subjects_panel_layout.addLayout(subject_action_btn_layout)
 
 		study_plan_ui_layout.addLayout(study_plan_panel_layout)
@@ -66,9 +118,9 @@ class MainWindow(QMainWindow):
 		# editing, and removing information of divisions, courses, etc., and
 		# invoking the scheduling process.
 		top_level_action_btn_layout = QHBoxLayout()
-		self.edit_information_btn = QPushButton('Edit School Information')
+		edit_information_btn = QPushButton('Edit School Information')
 		top_level_action_btn_layout.addStretch(1)
-		top_level_action_btn_layout.addWidget(self.edit_information_btn)
+		top_level_action_btn_layout.addWidget(edit_information_btn)
 		top_level_action_btn_layout.addStretch(1)
 
 		main_layout = QVBoxLayout()
@@ -76,3 +128,14 @@ class MainWindow(QMainWindow):
 		main_layout.addLayout(top_level_action_btn_layout)
 
 		frame.setLayout(main_layout)
+
+		# Connect the buttons to actions.
+		add_study_plan_btn.clicked.connect(self._add_study_plan_action)
+
+	@pyqtSlot()
+	def _add_study_plan_action(self):
+		action_dialog = AddRecordDialog().for_model(
+			'StudyPlan',
+			[ 'year_level', 'num_followers' ])
+		action_dialog.show()
+		action_dialog.exec_()
