5faa30fd39154d660805f2c215c6e6e8b03397d8
Sean Francis N. Ballais <sfballais123@gmail.com>
2019-11-08 19:16:39 +0800

    :recycle::lipstick: Each dialog that pops up is now a new instance of that dialog. Added a divider before the dialog action buttons. Modified the dialog generation code to more easily add and edit instances of models the interfacing dialog.

--- upvtc_ct/gui.py
+++ upvtc_ct/gui.py
@@ -1,212 +1,252 @@
+from dataclasses import dataclass
+
 import peewee
 from PyQt5.QtCore import QSize, QTime, pyqtSlot
 from PyQt5.QtWidgets import (
-	QMainWindow, QDialog, QFrame, QGridLayout, QHBoxLayout, QVBoxLayout,
-	QComboBox, QLabel, QLineEdit, QListWidget, QPushButton, QSpinBox,
-	QDoubleSpinBox, QTimeEdit)
+	QMainWindow, QDialog, QWidget, QFrame, QGridLayout, QHBoxLayout,
+	QVBoxLayout, QComboBox, QLabel, QLineEdit, QListWidget, QPushButton,
+	QSpinBox, QDoubleSpinBox, QTimeEdit)
 
 from upvtc_ct import models
 
 
-class AddRecordDialog():
-	cached_add_record_windows = dict()
+@dataclass
+class _RecordDialogWidget():
+	widget: QWidget
+	has_modified: bool = False
+
+
+class _RecordDialog(QDialog):
+	def __init__(self, parent=None):
+		super().__init__(parent)
+
+		self.widgets = dict()
+
+
+class RecordDialogFactory():
+	@classmethod
+	def create_dialog(cls, model, model_instance, attrs=[], title=None):
+		"""
+		Creates a new dialog for creating new or editing records of a model.
+		"""
+		dialog = _RecordDialog()
+		dialog.setModal(True)
+		dialog.setWindowTitle(title or f'Add {model}')
+
+		# Add in the GUI equivalents of the fields in the model.
+		dialog_layout = QVBoxLayout(dialog)
+		dialog.setLayout(dialog_layout)
+
+		m = getattr(models, model)
+		for attr in attrs:
+			attr_field = getattr(m, attr)
+
+			attr_type = type(attr_field)
+			attr_layout = None
+			capitalize_func = lambda s: s.capitalize()
+			attr_label = QLabel(
+					' '.join(list(map(capitalize_func, attr.split('_')))))
+			# TODO: Refactor these conditional blocks. Use a design
+			#       pattern. You may refer to the following link:
+			#           https://itnext.io/how-we-avoided-if-else-and-wrote
+			#           -extendable-code-with-strategy-pattern-256e34b90caf
+			if attr_type is peewee.CharField:
+				attr_layout = QHBoxLayout()
+
+				attr_layout.addWidget(attr_label)
+				attr_layout.addStretch(1)
+
+				attr_textfield = QLineEdit()
+				attr_layout.addWidget(attr_textfield)
+
+				dialog.widgets[attr] = _RecordDialogWidget(attr_textfield)
+			elif attr_type is peewee.SmallIntegerField:
+				attr_layout = QHBoxLayout()
+
+				attr_layout.addWidget(attr_label)
+				attr_layout.addStretch(1)
+
+				attr_spinbox = QSpinBox()
+				attr_spinbox.setMinimum(0)
+				attr_layout.addWidget(attr_spinbox)
+
+				dialog.widgets[attr] = _RecordDialogWidget(attr_spinbox)
+			elif attr_type is peewee.DecimalField:
+				attr_layout = QHBoxLayout()
+
+				attr_layout.addWidget(attr_label)
+				attr_layout.addStretch(1)
+
+				attr_spinbox = QDoubleSpinBox()
+				attr_spinbox.setMinimum(1.0)
+				attr_spinbox.setSingleStep(0.5)
+				attr_layout.addWidget(attr_spinbox)
+
+				dialog.widgets[attr] = _RecordDialogWidget(attr_spinbox)
+			elif attr_type is peewee.TimeField:
+				attr_layout = QHBoxLayout()
+
+				attr_layout.addWidget(attr_label)
+				attr_layout.addStretch(1)
+
+				attr_timefield = QTimeEdit(QTime(7, 0))
+				attr_timefield.setMinimumTime(QTime(7, 0))
+				# The time below is the start of the last timeslot
+				# of a day.
+				attr_timefield.setMaximumTime(QTime(18, 30))
+				attr_layout.addWidget(attr_timefield)
+
+				dialog.widgets[attr] = _RecordDialogWidget(attr_timefield)
+			elif attr_type is peewee.ForeignKeyField:
+				attr_layout = QVBoxLayout()
+
+				attr_layout.addWidget(attr_label)
+
+				attr_options = QComboBox()
+				attr_options.addItem('None', None)
+				linked_model = attr_field.rel_model
+				for record in linked_model.select():
+					attr_options.addItem(str(record), record)
+				attr_layout.addWidget(attr_options)
+
+				dialog.widgets[attr] = _RecordDialogWidget(attr_options)
+			elif attr_type is peewee.ManyToManyField:
+				attr_layout = QVBoxLayout()
+
+				attr_layout.addWidget(attr_label)
+
+				# Add the panel that lets you select linked model instances
+				# and add them to the instances the new model instance
+				# is linked to.
+				attr_add_options_layout = QGridLayout()
+				attr_add_options_layout.setColumnStretch(0, 1)
+
+				attr_options = QComboBox()
+				linked_model = attr_field.rel_model
+				for record in linked_model.select():
+					attr_options.addItem(str(record), record)
+
+				attr_add_option_btn = QPushButton('Add')
+
+				attr_add_options_layout.addWidget(attr_options, 0, 0)
+				attr_add_options_layout.addWidget(attr_add_option_btn, 0, 1)
+
+				attr_layout.addLayout(attr_add_options_layout)
+
+				# Add the list of linked model instances connected to the
+				# new model instance.
+				attr_linked_models = QListWidget()
+				attr_layout.addWidget(attr_linked_models)
+
+				# Add the action buttons for linked model instances.
+				attr_linked_models_action_btn_layout = QHBoxLayout()
+				attr_remove_linked_model_btn = QPushButton('Remove')
+				attr_edit_linked_model_btn = QPushButton('Edit')
+				attr_linked_models_action_btn_layout.addStretch(1)
+				attr_linked_models_action_btn_layout.addWidget(
+					attr_remove_linked_model_btn)
+				attr_linked_models_action_btn_layout.addWidget(
+					attr_edit_linked_model_btn)
+
+				attr_layout.addLayout(attr_linked_models_action_btn_layout)
+
+				dialog.widgets[attr] = _RecordDialogWidget(attr_linked_models)
+
+			dialog_layout.addLayout(attr_layout)
+
+		dialog_action_btn_layout = QHBoxLayout()
+		
+		save_btn = QPushButton('Save')
+		@pyqtSlot()
+		def save_action():
+			dialog.done(QDialog.Accepted)
+		save_btn.clicked.connect(save_action)
+
+		cancel_btn = QPushButton('Cancel')
+		@pyqtSlot()
+		def cancel_action():
+			dialog.done(QDialog.Rejected)
+		cancel_btn.clicked.connect(cancel_action)
+
+		dialog_action_btn_divider = DividerLineFactory.create_divider_line(
+			QFrame.HLine)
+		dialog_layout.addWidget(dialog_action_btn_divider)
+
+		dialog_action_btn_layout.addStretch(1)
+		dialog_action_btn_layout.addWidget(cancel_btn)
+		dialog_action_btn_layout.addWidget(save_btn)
+		dialog_layout.addLayout(dialog_action_btn_layout)
+
+		return dialog
+
+
+class DividerLineFactory():
+	@classmethod
+	def create_divider_line(cls, line_orientation):
+		line = QFrame()
+		line.setFrameShape(line_orientation)  # Should only accept
+											  # QFrame.VLine or QFrame.HLine.
+		line.setFrameShadow(QFrame.Raised)
+
+		return line
+
+
+class EditInformationWindow(QMainWindow):
+	_instance = None
+
+	def __init__(self):
+		super().__init__()
+
+		self.setMinimumSize(QSize(1280, 480))
+		self.setWindowTitle('Edit school information')
+
+		frame = QFrame(self)
+		self.setCentralWidget(frame)
+
+		model_information_layout = QHBoxLayout()
+
+		# Automatically create the panels for the specified models.
+		information_models = [ 'Division', 'Course', 'Subject', 'Room' ]
+		ctr = 0
+		for information_model in information_models:
+			model_layout = QVBoxLayout()
+
+			# Let's start with the label.
+			panel_title = QLabel(f'{information_model}s')
+			model_layout.addWidget(panel_title)
+
+			# And, of course, the list.
+			model_instances_list = QListWidget()
+			model_layout.addWidget(model_instances_list)
+
+			# And then the action buttons.
+			action_btn_layout = QHBoxLayout()
+
+			remove_btn = QPushButton('Remove')
+			edit_btn = QPushButton('Edit')
+			add_btn = QPushButton('Add')
+			action_btn_layout.addWidget(remove_btn)
+			action_btn_layout.addStretch(1)
+			action_btn_layout.addWidget(edit_btn)
+			action_btn_layout.addWidget(add_btn)
+			model_layout.addLayout(action_btn_layout)
+
+			model_information_layout.addLayout(model_layout)
+
+			if ctr < len(information_models) - 1:
+				model_information_layout.addWidget(
+					DividerLineFactory().create_divider_line(QFrame.VLine))
+
+			ctr += 1
+
+		frame.setLayout(model_information_layout)
 
 	@classmethod
-	def for_model(cls,
-				  model: str,
-				  attrs=[],
-				  title=None,
-				  save_func=None,
-				  cancel_func=None,
-				  reset_on_close=True):
-		"""
-		Creates a new dialog for creating new records of a model. If a dialog
-		has already been created beforehand, we use the previously created
-		dialog.
-		"""
-		if model in cls.cached_add_record_windows:
-			return cls.cached_add_record_windows[model]
-		else:
-			dialog = QDialog()
-			dialog.setModal(True)
-			dialog.setWindowTitle(title or f'Add {model}')
+	def get_instance(cls):
+		if EditInformationWindow._instance is None:
+			EditInformationWindow._instance = EditInformationWindow()
 
-			# Add in the GUI equivalents of the fields in the model.
-			dialog_layout = QVBoxLayout(dialog)
-			dialog.setLayout(dialog_layout)
-
-			m = getattr(models, model)
-			attr_widgets = dict()  # key -> (widget object, default value,)
-			for attr in attrs:
-				attr_field = getattr(m, attr)
-
-				attr_type = type(attr_field)
-				attr_layout = None
-				capitalize_func = lambda s: s.capitalize()
-				attr_label = QLabel(
-					' '.join(list(map(capitalize_func, attr.split('_')))))
-				# TODO: Refactor these conditional blocks. Use a design
-				#       pattern. You may refer to the following link:
-				#           https://itnext.io/how-we-avoided-if-else-and-wrote
-				#           -extendable-code-with-strategy-pattern-256e34b90caf
-				if attr_type is peewee.CharField:
-					attr_layout = QHBoxLayout()
-
-					attr_layout.addWidget(attr_label)
-					attr_layout.addStretch(1)
-
-					attr_textfield = QLineEdit()
-					attr_layout.addWidget(attr_textfield)
-
-					attr_widgets[attr] = (attr_textfield, '',)
-				elif attr_type is peewee.SmallIntegerField:
-					attr_layout = QHBoxLayout()
-
-					attr_layout.addWidget(attr_label)
-					attr_layout.addStretch(1)
-
-					attr_spinbox = QSpinBox()
-					attr_spinbox.setMinimum(0)
-					attr_layout.addWidget(attr_spinbox)
-
-					attr_widgets[attr] = (attr_spinbox, 0,)
-				elif attr_type is peewee.DecimalField:
-					attr_layout = QHBoxLayout()
-
-					attr_layout.addWidget(attr_label)
-					attr_layout.addStretch(1)
-
-					attr_spinbox = QDoubleSpinBox()
-					attr_spinbox.setMinimum(1.0)
-					attr_spinbox.setSingleStep(0.5)
-					attr_layout.addWidget(attr_spinbox)
-
-					attr_widgets[attr] = (attr_spinbox, 1.0,)
-				elif attr_type is peewee.TimeField:
-					attr_layout = QHBoxLayout()
-
-					attr_layout.addWidget(attr_label)
-					attr_layout.addStretch(1)
-
-					attr_timefield = QTimeEdit(QTime(7, 0))
-					attr_timefield.setMinimumTime(QTime(7, 0))
-					# The time below is the start of the last timeslot
-					# of a day.
-					attr_timefield.setMaximumTime(QTime(18, 30))
-					attr_layout.addWidget(attr_timefield)
-
-					attr_widgets[attr] = (attr_timefield, QTime(7, 0),)
-				elif attr_type is peewee.ForeignKeyField:
-					attr_layout = QVBoxLayout()
-
-					attr_layout.addWidget(attr_label)
-
-					attr_options = QComboBox()
-					attr_options.addItem('None', None)
-					linked_model = attr_field.rel_model
-					for record in linked_model.select():
-						attr_options.addItem(str(record), record)
-					attr_layout.addWidget(attr_options)
-
-					attr_widgets[attr] = (attr_options, None,)
-				elif attr_type is peewee.ManyToManyField:
-					attr_layout = QVBoxLayout()
-
-					attr_layout.addWidget(attr_label)
-
-					# Add the panel that lets you select linked model instances
-					# and add them to the instances the new model instance
-					# is linked to.
-					attr_add_options_layout = QHBoxLayout()
-					attr_options = QComboBox()
-					linked_model = attr_field.rel_model
-					for record in linked_model.select():
-						attr_options.addItem(str(record), record)
-
-					attr_add_option_btn = QPushButton('Add')
-
-					attr_add_options_layout.addWidget(attr_options)
-					attr_add_options_layout.addWidget(attr_add_option_btn)
-
-					attr_layout.addLayout(attr_add_options_layout)
-
-					attr_widgets[f'{attr}_options'] = (attr_options, None,)
-
-					# Add the list of linked model instances connected to the
-					# new model instance.
-					attr_linked_models = QListWidget()
-					attr_layout.addWidget(attr_linked_models)
-
-					attr_widgets[f'{attr}_linked_list'] = (
-						attr_linked_models, None,
-					)
-
-					# Add the action buttons for linked model instances.
-					attr_linked_models_action_btn_layout = QHBoxLayout()
-					attr_remove_linked_model_btn = QPushButton('Remove')
-					attr_edit_linked_model_btn = QPushButton('Edit')
-					attr_linked_models_action_btn_layout.addStretch(1)
-					attr_linked_models_action_btn_layout.addWidget(
-						attr_remove_linked_model_btn)
-					attr_linked_models_action_btn_layout.addWidget(
-						attr_edit_linked_model_btn)
-
-					attr_layout.addLayout(attr_linked_models_action_btn_layout)
-
-				dialog_layout.addLayout(attr_layout)
-
-			dialog_action_btn_layout = QHBoxLayout()
-			save_btn = QPushButton('Save')
-			@pyqtSlot()
-			def save_action():
-				if save_func is not None:
-					save_func()
-				dialog.done(QDialog.Accepted)
-
-			cancel_btn = QPushButton('Cancel')
-			@pyqtSlot()
-			def cancel_action():
-				if cancel_func is not None:
-					cancel_func()
-				dialog.done(QDialog.Rejected)
-			cancel_btn.clicked.connect(cancel_action)
-
-			dialog_action_btn_layout.addStretch(1)
-			dialog_action_btn_layout.addWidget(cancel_btn)
-			dialog_action_btn_layout.addWidget(save_btn)
-			dialog_layout.addLayout(dialog_action_btn_layout)
-
-			if reset_on_close:
-				@pyqtSlot()
-				def reset_widgets():
-					# TODO: Refactor these conditional blocks. Use a design
-					#       pattern. You may refer to the following link:
-					#           https://itnext.io/how-we-avoided-if-else-and-
-					#			wrote-extendable-code-with-strategy-pattern-
-					#           256e34b90caf
-					for _, attr_info in attr_widgets.items():
-						widget, default_widget_value = attr_info
-						if type(widget) is QLineEdit:
-							widget.setText(default_widget_value)
-						elif (type(widget) is QSpinBox
-							  or type(widget) is QDoubleSpinBox):
-							widget.setValue(default_widget_value)
-						elif type(widget) is QTimeEdit:
-							widget.setTime(default_widget_value)
-						elif type(widget) is QComboBox:
-							widget.setCurrentIndex(0)  # The item with an index
-													   # of 0 will always be
-													   # None.
-						elif type(widget) is QListWidget:
-							widget.clear()
-
-				dialog.finished.connect(reset_widgets)
-
-			# Add the model to the cache so that we don't have to keep on
-			# recreating the dialog.
-			cls.cached_add_record_windows[model] = dialog
-
-			return dialog
+		return EditInformationWindow._instance
 
 
 class MainWindow(QMainWindow):
@@ -300,12 +340,18 @@ class MainWindow(QMainWindow):
 
 		# Connect the buttons to actions.
 		add_study_plan_btn.clicked.connect(self._add_study_plan_action)
+		edit_information_btn.clicked.connect(
+			self._show_edit_information_window)
+
+	@pyqtSlot()
+	def _show_edit_information_window(self):
+		EditInformationWindow.get_instance().show()
 
 	@pyqtSlot()
 	def _add_study_plan_action(self):
-		action_dialog = AddRecordDialog().for_model(
+		action_dialog = RecordDialogFactory.create_dialog(
 			'StudyPlan',
+			None,
 			[ 'course', 'year_level', 'num_followers', 'subjects' ],
 			title='Add a new study plan')
 		action_dialog.show()
-		action_dialog.open()
