e2b7f7370a1af9b5366437ab62955b895d967d1e
Sean Francis N. Ballais <sfballais123@gmail.com>
2019-11-14 23:08:43 +0800

    :card_file_box: Managed to sort classes based on the number of candidate teachers their corresponding subjects have and printed each of the classes.

--- upvtc_ct/scheduler.py
+++ upvtc_ct/scheduler.py
@@ -1,3 +1,5 @@
+from peewee import fn
+
 from upvtc_ct import models
 
 
@@ -7,7 +9,18 @@ def assign_classes_to_teachers():
 	# with fewer number of candidate teachers will have their classes assigned
 	# teachers first to assign the class a teacher who is not likely overloaded
 	# from other subjects.
-	for subject_class in (models.Class
-							.select()
-							.order_by():
+	subject_teachers = models.Subject.candidate_teachers.get_through_model()
+	num_candidate_teachers = fn.COUNT(subject_teachers.teacher)
+	query = (subject_teachers
+				.select(
+					subject_teachers.subject.alias('teachers'),
+					num_candidate_teachers.alias('num_candidate_teachers'))
+				.group_by(subject_teachers.subject))
+	sorted_classes = (models.Class
+						.select(models.Class)
+						.join(
+							query,
+							on=(models.Class.subject == query.c.teachers))
+						.order_by(query.c.num_candidate_teachers.asc()))
+	for subject_class in sorted_classes:
 		print(subject_class)
