3019b80d4b36c8244416a14ae65ea5e0bdf964eb
Sean Francis N. Ballais <sfballais123@gmail.com>
2019-11-16 19:34:05 +0800

    :card_file_box: Managed to get the total number of units each teacher already has.

--- upvtc_ct/scheduler.py
+++ upvtc_ct/scheduler.py
@@ -1,4 +1,4 @@
-from peewee import fn
+from peewee import fn, Select, JOIN
 
 from upvtc_ct import models
 
@@ -13,14 +13,39 @@ def assign_classes_to_teachers():
 	num_candidate_teachers = fn.COUNT(subject_teachers.teacher)
 	query = (subject_teachers
 				.select(
-					subject_teachers.subject.alias('teachers'),
+					subject_teachers.subject.alias('subject'),
 					num_candidate_teachers.alias('num_candidate_teachers'))
 				.group_by(subject_teachers.subject))
 	sorted_classes = (models.Class
 						.select(models.Class)
 						.join(
 							query,
-							on=(models.Class.subject == query.c.teachers))
+							on=(models.Class.subject == query.c.subject))
 						.order_by(query.c.num_candidate_teachers.asc()))
 	for subject_class in sorted_classes:
+		class_units = (Select(columns=[
+							models.Class.id,
+							models.Class.assigned_teacher.alias(
+								'assigned_teacher'),
+							models.Subject.units.alias('units')
+						])
+						.from_(models.Class, models.Subject)
+						.where(models.Class.subject == models.Subject.id))
+		teacher_units = (Select(columns=[
+								models.Teacher.id.alias('assigned_teacher'),
+								fn.COALESCE(
+									fn.SUM(class_units.c.units),
+									0).alias('units')
+							])
+							.from_(models.Teacher)
+							.join(
+								class_units,
+								JOIN.LEFT_OUTER,
+								on=(models.Teacher.id
+									== class_units.c.assigned_teacher))
+							.group_by(models.Teacher.id))
+		print(teacher_units.sql())
+		print('---')
 		print(subject_class)
+		for teacher in teacher_units.execute(models.db):
+			print(teacher)
