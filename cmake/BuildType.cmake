b68769990d93fcc9f48c100200087b56a8013c81
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-02-27 22:07:04 +0800

    :building_construction: Initial base for the project.

---
+++ cmake/BuildType.cmake
@@ -0,0 +1,14 @@
+# NOTE (Feb. 27, 2020): From my other project, Planes.
+# Specifies the build type of this project.
+# The default build type of this project is "debug", since debug compilations
+# will be more frequent than release compilations.
+
+if (NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE Debug)
+endif()
+
+if (CMAKE_BUILD_TYPE STREQUAL Debug)
+    add_compile_definitions(DEBUG)
+endif()
+
+message(STATUS "Build Type: ${CMAKE_BUILD_TYPE}")
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "I'm working!" << std::endl;
+
+  return 0;
+}
